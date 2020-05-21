6b2e4f0b6a6233f77af693714bdbc1b57a35c29f
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2020-05-21 11:07:48 +0200

    first commit

---
+++ spring-boot-angular-jenkins-rest/src/main/java/com/qugenx/jenkins/rest/SpringBootAngularJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.qugenx.jenkins.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAngularJenkinsApplication.class, args);
+    }
+
+}
